0e07a4f4a802ad58c5683135e0453043b732413e
IlkinNehmedov <129421257+IlkinNehmedov@users.noreply.github.com>
2023-04-13 17:58:55 +0400

    Update edaletconfig.py

--- Plugins/komekci/edaletconfig.py
+++ Plugins/komekci/edaletconfig.py
@@ -2,9 +2,9 @@ from telethon import TelegramClient
 # Config məlumatları
 
 # Telegram Client (Telethon)
-API_ID = 23255992
-API_HASH = "da1c7ce7cfe5cd7e5ebb09cfc981b975"
-bot_token = "6186814134:AAGEhd-iLFEeMIpLRiucFA3KjsogGzvix2o"
+API_ID = 26041892
+API_HASH = "b25edf599fd7a94f8d84ce3e4422f409"
+bot_token = "6041859157:AAHNOk1endHo7m_U48vNdNaASYyL0Y3HcDw"
 
 edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
 
