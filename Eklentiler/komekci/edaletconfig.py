9ae28c2757b9e6a84befdbb143421eb2d46a1926
Ruslann21 <118375684+Ruslann21@users.noreply.github.com>
2023-04-18 02:55:03 +0400

    Update and rename Plugins/komekci/edaletconfig.py to Eklentiler/komekci/edaletconfig.py
    Hasbudur

--- Eklentiler/komekci/edaletconfig.py
+++ Eklentiler/komekci/edaletconfig.py
@@ -2,9 +2,9 @@ from telethon import TelegramClient
 # Config məlumatları
 
 # Telegram Client (Telethon)
-API_ID = 26041892
-API_HASH = "b25edf599fd7a94f8d84ce3e4422f409"
-bot_token = "6041859157:AAHNOk1endHo7m_U48vNdNaASYyL0Y3HcDw"
+API_ID = 22007754
+API_HASH = "7f0d6b4d928155eda014acdb5a014620"
+bot_token = "5738149102:AAHzIVHlo1WD6Hn9atLyHjuuVEqDKZTcHJA"
 
 edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
 
