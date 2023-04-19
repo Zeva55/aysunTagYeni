ea99f128b03aeb233bfc52779701d6616e3aeaea
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-04-19 22:32:50 +0400

    Delete __init__.py

--- Plugins/__init__.py
+++
@@ -1,6 +0,0 @@
-from telethon import TelegramClient 
-import logging
-from Plugins.komekci.edaletconfig import edalet
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.INFO)
