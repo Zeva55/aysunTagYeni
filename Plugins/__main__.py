7a03e253d3b946069cec971b25bfc29f259a3ff5
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-04-19 23:06:58 +0400

    Update __main__.py

--- Plugins/__main__.py
+++ Plugins/__main__.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from Plugins.utils import load_edaletbalaev
 import logging
 from Plugins import edaletbalaev
-from Plugins.komekci.edaletconfig import edalet
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
